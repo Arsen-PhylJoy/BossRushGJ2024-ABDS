7af97db556afc7f70f9af7eff062e12895fda5f5
PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-01-09 15:07:11 +0300

    Add loading screen scene an level manager global node

---
+++ _debug/test_transition_level_system/scenes/levels/test_level_1.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _ready() -> void:
+	$Button.connect("pressed",on_go_to_menu_pressed)
+	
+func on_go_to_menu_pressed():
+	LevelManager.load_level("res://_debug/test_transition_level_system/scenes/levels/test_menu.tscn")
