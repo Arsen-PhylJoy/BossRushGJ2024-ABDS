84012dab7aab9ece7ca44e78b409b44761e0d2ab
Arseny Semenov <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-01-18 20:37:55 +0300

    Range attack
    Add range attack spawner
    Connect range_attack function to the boss and connect range_attack_spwaner to it

--- scenes/enemies/Bullet/bullet.gd
+++ scenes/enemies/Bullet/bullet.gd
@@ -3,21 +3,21 @@ extends RigidBody2D
 
 @export var damange:float = 30.0
 @export var explosion_VFX: PackedScene = preload("res://scenes/VFX/bullet_exposion.tscn")
+@onready var _damage_area: Area2D =$DamageArea2D
 @onready var _visible_notifier : VisibleOnScreenNotifier2D = $BulletVisibleOnScreenNotifier2D
-@onready var damage_area: Area2D = $DamageArea2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready()->void:
-	if _visible_notifier.screen_exited.connect(_on_visible_on_screen_notifier_2d_screen_exited): printerr("Fail: ",get_stack()) 
-	if damage_area.area_entered.connect(_on_body_entered): printerr("Fail: ",get_stack())
-	
-func _on_visible_on_screen_notifier_2d_screen_exited()->void:
-	queue_free()
+	if _visible_notifier.screen_exited.connect(queue_free): printerr("Fail: ",get_stack()) 
+	if _damage_area.area_entered.connect(_on_body_entered): printerr("Fail: ",get_stack())
+
 
 func _on_body_entered(area: Area2D)->void:
+	if( area.is_in_group("Enemy")):
+		return
 	var explosion_VFX_instance: GPUParticles2D = explosion_VFX.instantiate() as GPUParticles2D
 	get_tree().current_scene.add_child(explosion_VFX_instance)
-	explosion_VFX_instance.emitting = true;
-	explosion_VFX_instance.global_position = area.global_position
+	explosion_VFX_instance.global_position = self.global_position
+	explosion_VFX_instance.restart()
 	queue_free()
--- scenes/enemies/bosses/first_boss/range_attack/range_attacks_spawner.gd
+++ scenes/enemies/bosses/first_boss/range_attack/range_attacks_spawner.gd
@@ -1,18 +1,28 @@
 class_name RangeAttackSpawner
 extends Node2D
 
-@export var spawn_radius: float = 100.0
-@export var intial_speed: float = 100.0
-@export var accuracy: float = 50.0
+@export var initial_speed: float = 20.0
+@export var wave_cooldown: float = 0.3
 @export var waves: int = 5
-@export var bullets_in_wave: int = 4
 @onready var bullet_pc: PackedScene = preload("res://scenes/enemies/bullet/bullet.tscn")
 
-func spawn_bullets(position: Vector2, aim_direction:Vector2)->void:
-	pass
+func attack(central_point:Vector2, aim_points: Array[Vector2])->void:
+	for i: int in waves:
+		var bullets: Array[Bullet] = _create_bullets(aim_points)
+		_launch_bullets(bullets, central_point)
+		await get_tree().create_timer(wave_cooldown).timeout
 	
-	
-func  _create_bullet(aim_direction:Vector2)->Bullet:
-	var bullet: Bullet = Bullet.new()
-	bullet.apply_force()
-	return bullet
+
+func  _create_bullets(spawn_positions:Array[Vector2])->Array[Bullet]:
+	var bullets: Array[Bullet] = []
+	for pos: Vector2 in spawn_positions:
+		var bullet: Bullet = bullet_pc.instantiate()
+		get_tree().current_scene.add_child(bullet)
+		bullet.global_position = pos
+		bullets.append(bullet)
+	return bullets
+
+func _launch_bullets(bullets: Array[Bullet] ,cental_point: Vector2)->void:
+	for bullet: Bullet in bullets:
+		var direction: Vector2 = bullet.global_position - cental_point
+		bullet.apply_impulse(direction*initial_speed)
