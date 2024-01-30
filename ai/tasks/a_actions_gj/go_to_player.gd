0a102de7ac86e9934d920e5c14325abea402a847
Arseny Semenov <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-01-30 21:04:26 +0300

    Expand boss's interface and rework animations

--- ai/tasks/a_actions_gj/go_to_player.gd
+++ ai/tasks/a_actions_gj/go_to_player.gd
@@ -8,4 +8,6 @@ func _enter()->void:
 func _tick(_delta: float) -> Status:
 	if(blackboard.get_data().get("is_moved_to_player") as bool == false):
 		(agent as FirstBoss).set_movement_target((blackboard.get_data().get("player") as PlayerCharacter).global_position)
-	return RUNNING
+		return RUNNING
+	else:
+		return SUCCESS
