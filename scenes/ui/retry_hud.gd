8bcdec535b34d6806673e9b9a8734067b5594265
Arseny Semenov <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-02-25 20:17:22 +0300

    retry window

--- scenes/ui/retry_hud.gd
+++ scenes/ui/retry_hud.gd
@@ -5,19 +5,10 @@ extends CanvasLayer
 @onready var _load_level_path: String
 
 func _ready() -> void:
-	if (self as Node).tree_exiting.connect(_on_free): printerr("Fail: ",get_stack())
 	if _retry_button.pressed.connect(_on_retry_pressed): printerr("Fail: ",get_stack())
 	get_tree().paused = true
 
-func _on_continue_pressed()->void:
-	get_tree().paused = false
-	queue_free()
-
 func _on_retry_pressed()->void:
 	LevelManager.load_level("res://scenes/levels/2_boss_fight/2_boss_fight.tscn")
-
-func _on_exit_pressed()->void:
-	get_tree().quit()
-
-func _on_free()->void:
 	get_tree().paused = false
+	queue_free()
